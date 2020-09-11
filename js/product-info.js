23841ede2674052e75e040dc24145c5efa1cd91e
ronin-21 <68908198+ronin-21@users.noreply.github.com>
2020-09-11 11:03:21 -0300

    Agregado de productos relacionados

--- js/product-info.js
+++ js/product-info.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
         
 
         showProdInfo(producto);
+        showRelatedProducts(producto.relatedProducts)
     })
 
 });
@@ -14,11 +15,14 @@ document.addEventListener("DOMContentLoaded", function (e) {
 function showProdInfo(producto){
     const prodInfo = document.getElementById("prodInfo");
     let htmlToAppend = `
-    <div class="card" style="width: 18rem;">
+    
+    <div class="card p-2 bd-highlight" style="width: 450px;">
     <img src="${producto.images[0]}" class="card-img-top" alt="">
     <div class="card-body">
-      <h5 class="card-title" id="prodTitle">${producto.name}</h5>
-      <p class="card-text" id="prodDescription">${producto.description}</p>
+      <h5 class="card-title">${producto.name}</h5>
+      <p class="card-text">${producto.currency} ${producto.cost} </p>
+      <p class="card-text">Stock: ${producto.soldCount} Unidades</p>      
+      <p class="card-text">${producto.description}</p>
 
       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
@@ -51,9 +55,44 @@ function showProdInfo(producto){
       </a>
     </div>
 
-      <a href="" class="btn btn-primary">View</a>
+    <p class="card-text"> Categoria: ${producto.category}</p>
+    
+
+
+      
     </div>
     </div>
     `;
     prodInfo.innerHTML = htmlToAppend;
+}
+
+function showRelatedProducts(array) {
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+      if (resultObj.status === "ok") {
+          productsList = resultObj.data;
+
+          let htmlRelatedProducts = "";
+
+          for (let i = 0; i < array.length; i++) {
+              let rcomment = array[i];
+              let relatedProducts = productsList[rcomment];
+
+              htmlRelatedProducts += `
+              <div class= "col-lg-3 col-md-4 col-6 border">
+              
+                  <div id="relatedVideogameImg" class= "row">
+                      <img class="img-fluid p-2" src="`+ relatedProducts.imgSrc +`">                                              
+                  </div>                   
+                  <div "relatedVideogameInfo" class= "row p-2">
+                  <p>`+ relatedProducts.name + `</p> 
+                  <p>`+ relatedProducts.description + `</p>
+                  </div>
+                  <div class= "row p-2">
+                  <a href="videogames-info.html">Ver</a>
+                  </div>                     
+              </div>`
+          }
+          document.getElementById("relatedProducts").innerHTML = htmlRelatedProducts;
+      }
+  })
 }
