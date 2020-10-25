e26b8b3400000954480413e1a307db16b13ac7e4
ronin-21 <68908198+ronin-21@users.noreply.github.com>
2020-10-25 17:48:40 -0300

    -Creacion de funciones para corroborar datos -Creacion de funcion para calculo de envio

--- js/cart.js
+++ js/cart.js
@@ -22,6 +22,8 @@ function updateAllSubTotal(){
     document.getElementById("subtotalText").innerHTML = "UYU " + subtotal;
     subtotaltotal=subtotal; 
 
+    return subtotal;
+
 }
 
 function calcTotal(){
@@ -36,6 +38,7 @@ function addEventCount(){
         document.getElementById("productSubtotal-"+i).innerHTML= arrayArticles[i].currency + " "+subtotalArray[i].value* arrayArticles[i].unitCost;
         updateAllSubTotal();
         calcTotal();
+        calcShipping();
     });
 
     }
@@ -76,3 +79,85 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 })
+
+
+
+function calcShipping(){
+    //Funcion que calcula y muestra los diferentes costos del envio reflejados en el total y subtotal
+    let premium = document.getElementById("premium");
+    let express = document.getElementById("express");
+    let standard = document.getElementById("standard");
+    let subtotal = updateAllSubTotal();
+
+    let envio=0;
+    let total=0;
+
+    if(premium.checked){
+        console.log(updateAllSubTotal());
+        envio = (subtotal) * 0.15;
+        
+    } 
+
+    if(express.checked){
+        console.log(updateAllSubTotal());
+        envio = (subtotal) * 0.07;
+        
+    }
+    
+    if(standard.checked){
+        console.log(updateAllSubTotal());
+        envio = (subtotal) * 0.05;
+        
+    }
+     
+    total = envio + updateAllSubTotal();
+    document.getElementById("shippinCost").innerHTML = "UYU "+ envio;
+    document.getElementById("totalCostText").innerHTML = "UYU "+ total;
+}
+
+function corroborate(){
+    //Funcion que corrobora que todos los datos de la direccion esten completos uno por uno
+    if(document.getElementById("shippinCost").innerHTML === ""){
+        alert("Seleccione un envio");
+        return false;
+    } else if(document.getElementById("street").value === "" || document.getElementById("street2").value === ""){
+        alert("Ingrese las calles para el envio");
+        return false;
+    } else if(document.getElementById("number").value === ""){
+        alert("Ingrese número de puerta");
+        return false;
+    } else if(document.getElementById("numberApt").value === ""){
+        alert("Ingrese apartamento");
+        return false;
+    }
+}
+
+function corroborateModal(){
+    //Funcion que corrobora que todos los datos del modal esten completos uno por uno
+    let master = document.getElementById("exampleCheck1").checked;
+    let visa = document.getElementById("exampleCheck2").checked;
+
+    if(document.getElementById("name").value === "" || document.getElementById("email").value === ""){
+        alert("Ingrese Nombre y Email");
+        return false;
+    } else if(!master && !visa){
+        alert("Seleccione una tarjeta");
+        return false;
+    } else if(document.getElementById("numTarjeta").value === "" || document.getElementById("securityNum").value === "" || document.getElementById("cardExpiration").value === ""){
+        alert("Ingrese los datos correspondientes de la tarjeta");
+        return false;
+    }
+
+    correctPay();
+}
+
+function correctPay(){
+        let msj;
+        getJSONData(CART_BUY_URL).then(result => {
+            if (result.status === "ok") {
+                console.log(result.data.msg);
+                msj = result.data.msg
+            }
+            alert(msj);
+        })
+}
