409498afc43dd859e6cefcc080dda9f4dcf522da
ronin-21 <68908198+ronin-21@users.noreply.github.com>
2020-11-03 08:37:50 -0300

    Empezando cambios en pagina de inicio

--- js/products.js
+++ js/products.js
@@ -47,7 +47,7 @@ function showProductList(){
             htmlContentToAppend += `
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
-                    <div class="col-3">
+                    <div class="col-sm">
                         <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                     </div>
                     <div class="col">
